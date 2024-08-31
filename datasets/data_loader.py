1131f2ca97f63510a76953f685cd6d1d717a63b7
litianze <fl1123@ic.ac.uk>
2024-08-31 14:21:59 +0000

    update TransVG-CLIP

--- datasets/data_loader.py
+++ datasets/data_loader.py
@@ -18,18 +18,22 @@ import numpy as np
 import os.path as osp
 import scipy.io as sio
 import torch.utils.data as data
+
 sys.path.append('.')
+
 
 from PIL import Image
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 from utils.word_utils import Corpus
 
+# from CLIP-VG.models.clip import *
+import clip
 
 def read_examples(input_line, unique_id):
     """Read a list of `InputExample`s from an input file."""
     examples = []
     # unique_id = 0
-    line = input_line #reader.readline()
+    line = input_line  # reader.readline()
     # if not line:
     #     break
     line = line.strip()
@@ -46,6 +50,7 @@ def read_examples(input_line, unique_id):
     # unique_id += 1
     return examples
 
+
 ## Bert text encoding
 class InputExample(object):
     def __init__(self, unique_id, text_a, text_b):
@@ -53,14 +58,17 @@ class InputExample(object):
         self.text_a = text_a
         self.text_b = text_b
 
+
 class InputFeatures(object):
     """A single set of features of data."""
+
     def __init__(self, unique_id, tokens, input_ids, input_mask, input_type_ids):
         self.unique_id = unique_id
         self.tokens = tokens
         self.input_ids = input_ids
         self.input_mask = input_mask
         self.input_type_ids = input_type_ids
+
 
 def convert_examples_to_features(examples, seq_length, tokenizer):
     """Loads a data file into a list of `InputBatch`s."""
@@ -122,35 +130,38 @@ def convert_examples_to_features(examples, seq_length, tokenizer):
                 input_type_ids=input_type_ids))
     return features
 
+
 class DatasetNotFoundError(Exception):
     pass
 
+
 class TransVGDataset(data.Dataset):
     SUPPORTED_DATASETS = {
-        'referit': {'splits': ('train', 'val', 'trainval', 'test')},
+        'referit': {'splits': ('train', 'val', 'trainval', 'test', 'train_pseudo')},
         'unc': {
-            'splits': ('train', 'val', 'trainval', 'testA', 'testB'),
+            'splits': ('train', 'val', 'trainval', 'testA', 'testB', 'train_pseudo'),
             'params': {'dataset': 'refcoco', 'split_by': 'unc'}
         },
         'unc+': {
-            'splits': ('train', 'val', 'trainval', 'testA', 'testB'),
+            'splits': ('train', 'val', 'trainval', 'testA', 'testB', 'train_pseudo'),
             'params': {'dataset': 'refcoco+', 'split_by': 'unc'}
         },
         'gref': {
-            'splits': ('train', 'val'),
+            'splits': ('train', 'val', 'train_pseudo'),
             'params': {'dataset': 'refcocog', 'split_by': 'google'}
         },
         'gref_umd': {
-            'splits': ('train', 'val', 'test'),
+            'splits': ('train', 'val', 'test', 'train_pseudo'),
             'params': {'dataset': 'refcocog', 'split_by': 'umd'}
         },
         'flickr': {
-            'splits': ('train', 'val', 'test')}
+            'splits': ('train', 'val', 'test', 'train_pseudo')}
     }
 
-    def __init__(self, data_root, split_root='data', dataset='referit', 
+    """ the core part of the dataset processing """
+    def __init__(self, data_root, split_root='data', dataset='referit',
                  transform=None, return_idx=False, testmode=False,
-                 split='train', max_query_len=128, lstm=False, 
+                 split='train', max_query_len=128, prompt_template=None, lstm=False,
                  bert_model='bert-base-uncased'):
         self.images = []
         self.data_root = data_root
@@ -158,15 +169,16 @@ class TransVGDataset(data.Dataset):
         self.dataset = dataset
         self.query_len = max_query_len
         self.lstm = lstm
+        self.prompt_template = prompt_template
         self.transform = transform
         self.testmode = testmode
         self.split = split
-        self.tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
-        self.return_idx=return_idx
+        # self.tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=True)
+        self.return_idx = return_idx
 
         assert self.transform is not None
 
-        if split == 'train':
+        if split in ['train', 'train_pseudo']:
             self.augment = True
         else:
             self.augment = False
@@ -175,17 +187,17 @@ class TransVGDataset(data.Dataset):
             self.dataset_root = osp.join(self.data_root, 'referit')
             self.im_dir = osp.join(self.dataset_root, 'images')
             self.split_dir = osp.join(self.dataset_root, 'splits')
-        elif  self.dataset == 'flickr':
+        elif self.dataset == 'flickr':
             self.dataset_root = osp.join(self.data_root, 'Flickr30k')
-            self.im_dir = osp.join(self.dataset_root, 'flickr30k_images')
-        else:   ## refcoco, etc.
+            # TODO: it should be note that this needs to change flickr30k_images to flickr30k-images
+            self.im_dir = osp.join(self.dataset_root, 'flickr30k-images')
+        else:  ## refcoco, etc.
             self.dataset_root = osp.join(self.data_root, 'other')
-            self.im_dir = osp.join(
-                self.dataset_root, 'images', 'mscoco', 'images', 'train2014')
+            self.im_dir = osp.join(self.dataset_root, 'images', 'mscoco', 'images', 'train2014')
             self.split_dir = osp.join(self.dataset_root, 'splits')
 
         if not self.exists_dataset():
-            # self.process_dataset()
+            print('The dataset {} is not found!'.format(osp.join(self.split_root, self.dataset)))
             print('Please download index cache to data folder: \n \
                 https://drive.google.com/open?id=1cZI562MABLtAzM6YU4WmKPFFguuVr0lZ')
             exit(0)
@@ -211,33 +223,32 @@ class TransVGDataset(data.Dataset):
             imgset_path = osp.join(dataset_path, imgset_file)
             self.images += torch.load(imgset_path)
 
+        if self.prompt_template:
+            self.images = self.prompt(self.images)
+
     def exists_dataset(self):
         return osp.exists(osp.join(self.split_root, self.dataset))
 
+    # TODO: 这句是关键
     def pull_item(self, idx):
         if self.dataset == 'flickr':
             img_file, bbox, phrase = self.images[idx]
         else:
             img_file, _, bbox, phrase, attri = self.images[idx]
         ## box format: to x1y1x2y2
+        bbox_ori = bbox
         if not (self.dataset == 'referit' or self.dataset == 'flickr'):
             bbox = np.array(bbox, dtype=int)
-            bbox[2], bbox[3] = bbox[0]+bbox[2], bbox[1]+bbox[3]
+            bbox[2], bbox[3] = bbox[0] + bbox[2], bbox[1] + bbox[3]
         else:
             bbox = np.array(bbox, dtype=int)
 
         img_path = osp.join(self.im_dir, img_file)
         img = Image.open(img_path).convert("RGB")
-        # img = cv2.imread(img_path)
-        # ## duplicate channel if gray image
-        # if img.shape[-1] > 1:
-        #     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # else:
-        #     img = np.stack([img] * 3)
 
         bbox = torch.tensor(bbox)
         bbox = bbox.float()
-        return img, phrase, bbox 
+        return img_file, img, phrase, bbox, bbox_ori
 
     def tokenize_phrase(self, phrase):
         return self.corpus.tokenize(phrase, self.query_len)
@@ -245,36 +256,67 @@ class TransVGDataset(data.Dataset):
     def untokenize_word_vector(self, words):
         return self.corpus.dictionary[words]
 
+    # TODO: 新增
+    def prompt(self, sample_list):
+        n = len(sample_list)
+        new_sample_list = []
+
+        for i in range(n):
+            if self.dataset == 'flickr':
+                tmp_sample = (sample_list[i][0], sample_list[i][1], self.prompt_template.replace('{pseudo_query}', sample_list[i][2]))
+            else:
+                # print("\nsample_list:\n", sample_list[i])
+                #  ('COCO_train2014_000000000839.jpg', '482127.pth', [303.58, 69.03, 293.29, 425.79],
+                #  'guy flopping around on the right', [('r1', ['guy']), ('r2', ['none']), ('r3', ['none']),
+                #  ('r4', ['none']), ('r5', ['none']), ('r6', ['none']), ('r7', ['none']),
+                #  ('r8', ['right', 'flopping', 'around'])])
+                tmp_sample = (sample_list[i][0], sample_list[i][1], sample_list[i][2],
+                              self.prompt_template.replace('{pseudo_query}', sample_list[i][3]), sample_list[i][4])
+            new_sample_list.append(tmp_sample)
+        return new_sample_list
+
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img, phrase, bbox = self.pull_item(idx)
-        # phrase = phrase.decode("utf-8").encode().lower()
+        img_file, img, phrase, bbox, bbox_ori = self.pull_item(idx)
         phrase = phrase.lower()
         input_dict = {'img': img, 'box': bbox, 'text': phrase}
         input_dict = self.transform(input_dict)
+
         img = input_dict['img']
+        img_mask = input_dict['mask']
         bbox = input_dict['box']
         phrase = input_dict['text']
-        img_mask = input_dict['mask']
-        
-        if self.lstm:
-            phrase = self.tokenize_phrase(phrase)
-            word_id = phrase
-            word_mask = np.array(word_id>0, dtype=int)
-        else:
-            ## encode phrase to bert input
-            examples = read_examples(phrase, idx)
-            features = convert_examples_to_features(
-                examples=examples, seq_length=self.query_len, tokenizer=self.tokenizer)
-            word_id = features[0].input_ids
-            word_mask = features[0].input_mask
-        
+
+        # if self.lstm:
+        #     phrase = self.tokenize_phrase(phrase)
+        #     word_id = phrase
+        #     word_mask = np.array(word_id > 0, dtype=int)
+        # else:
+        #     ## encode phrase to bert input
+        #     examples = read_examples(phrase, idx)
+        #     features = convert_examples_to_features(
+        #         examples=examples, seq_length=self.query_len, tokenizer=self.tokenizer)
+        #     word_id = features[0].input_ids
+        #     word_mask = features[0].input_mask
+
+        text_token = clip.tokenize(phrase)  # 1*77
+        text = text_token.int()[0].tolist()
+        text_mask = (text_token.clone() > 0).int()[0].tolist()
+        """ # old code
         if self.testmode:
             return img, np.array(word_id, dtype=int), np.array(word_mask, dtype=int), \
-                np.array(bbox, dtype=np.float32), np.array(ratio, dtype=np.float32), \
-                np.array(dw, dtype=np.float32), np.array(dh, dtype=np.float32), self.images[idx][0]
+                   np.array(bbox, dtype=np.float32), np.array(ratio, dtype=np.float32), \
+                   np.array(dw, dtype=np.float32), np.array(dh, dtype=np.float32), self.images[idx][0]
         else:
             # print(img.shape)
             return img, np.array(img_mask), np.array(word_id, dtype=int), np.array(word_mask, dtype=int), np.array(bbox, dtype=np.float32)
+        """
+
+        if self.testmode:  # default is False
+            return img, np.array(text, dtype=int), np.array(text_mask, dtype=int), \
+                   np.array(bbox, dtype=np.float32), np.array(ratio, dtype=np.float32), \
+                   np.array(dw, dtype=np.float32), np.array(dh, dtype=np.float32), self.images[idx][0]
+        else:
+            return img, np.array(img_mask), np.array(text, dtype=int), np.array(text_mask, dtype=int), np.array(bbox, dtype=np.float32), img_file, phrase, bbox_ori
--- models/trans_vg.py
+++ models/trans_vg.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from pytorch_pretrained_bert.modeling import BertModel
-from .visual_model.detr import build_detr
-from .language_model.bert import build_bert
+from .CLIP import build_clip
 from .vl_transformer import build_vl_transformer
 from utils.box_utils import xywh2xyxy
 
@@ -17,45 +16,49 @@ class TransVG(nn.Module):
         self.num_visu_token = int((args.imsize / divisor) ** 2)
         self.num_text_token = args.max_query_len
 
-        self.visumodel = build_detr(args)
-        self.textmodel = build_bert(args)
+        self.clip, _ = build_clip(args)
+        self.clip = self.clip.float().to(args.device)
+        self.visumodel = self.clip.visual
+        # self.visumodel = build_detr(args)
+        # self.textmodel = build_bert(args)
 
         num_total = self.num_visu_token + self.num_text_token + 1
         self.vl_pos_embed = nn.Embedding(num_total, hidden_dim)
         self.reg_token = nn.Embedding(1, hidden_dim)
 
-        self.visu_proj = nn.Linear(self.visumodel.num_channels, hidden_dim)
-        self.text_proj = nn.Linear(self.textmodel.num_channels, hidden_dim)
+        self.visu_proj = nn.Linear(self.visumodel.proj.shape[1], hidden_dim)
+        self.text_proj = nn.Linear(self.visumodel.proj.shape[1], hidden_dim)
 
         self.vl_transformer = build_vl_transformer(args)
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
 
+    def preprocess(self, img_data):
+        # preprocess img_data as NestedTensor
+        return F.interpolate(img_data.tensors, size=(224, 224), mode='bilinear', align_corners=False)
+
 
     def forward(self, img_data, text_data):
         bs = img_data.tensors.shape[0]
+        preprocessed_img_data = self.preprocess(img_data)
 
-        # visual backbone
-        visu_mask, visu_src = self.visumodel(img_data)
-        visu_src = self.visu_proj(visu_src) # (N*B)xC
+        # visual encoder
+        visu_src = self.visumodel(preprocessed_img_data)
+        visu_src = self.visu_proj(visu_src)
 
-        # language bert
-        text_fea = self.textmodel(text_data)
-        text_src, text_mask = text_fea.decompose()
-        assert text_mask is not None
+        # language encoderd
+        text_src = self.clip.encode_text(text_data.tensors)
         text_src = self.text_proj(text_src)
-        # permute BxLenxC to LenxBxC
-        text_src = text_src.permute(1, 0, 2)
-        text_mask = text_mask.flatten(1)
 
         # target regression token
         tgt_src = self.reg_token.weight.unsqueeze(1).repeat(1, bs, 1)
-        tgt_mask = torch.zeros((bs, 1)).to(tgt_src.device).to(torch.bool)
+
+        print(tgt_src.shape, text_src.shape, visu_src.shape)
         
         vl_src = torch.cat([tgt_src, text_src, visu_src], dim=0)
-        vl_mask = torch.cat([tgt_mask, text_mask, visu_mask], dim=1)
         vl_pos = self.vl_pos_embed.weight.unsqueeze(1).repeat(1, bs, 1)
+        print(vl_pos.shape)
 
-        vg_hs = self.vl_transformer(vl_src, vl_mask, vl_pos) # (1+L+N)xBxC
+        vg_hs = self.vl_transformer(vl_src, None, vl_pos) # (1+L+N)xBxC
         vg_hs = vg_hs[0]
 
         pred_box = self.bbox_embed(vg_hs).sigmoid()
